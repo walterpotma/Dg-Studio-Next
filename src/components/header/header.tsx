ca4b0245db262e87145206b8fad1d438e571faaf
walter potma <145157901+waltindobne@users.noreply.github.com>
2024-06-11 16:13:22 -0300

    correção erros de build

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
 				</div>
 				<nav className={styles.navigation}>
 					<button>
-						<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-align-justify"><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="18" y2="18"/></svg>
+						<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="18" y2="18"/></svg>
 					</button>
 				</nav>
 		</main>
