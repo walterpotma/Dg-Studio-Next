54dd8ed8567e815026b8b4a5b86ac066d94a9003
walter potma <145157901+waltindobne@users.noreply.github.com>
2024-07-16 14:15:41 -0300

    google ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,8 @@ export default function RootLayout({
     <html lang="pt-br">
       <head>
         <link rel="shortcut icon" href="https://cdn0.iconfinder.com/data/icons/back-to-school/90/circle-school-learn-study-subject-literature-book-512.png" type="image/x-icon" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8154336835421480"
+        crossOrigin="anonymous"></script>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
