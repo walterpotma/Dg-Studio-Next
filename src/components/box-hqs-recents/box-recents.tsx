9f6a3257af6e588c1bd2a042e47b024103b06875
walter potma <145157901+waltindobne@users.noreply.github.com>
2024-08-07 21:50:48 -0300

    pesquisa

--- src/components/box-hqs-recents/box-recents.tsx
+++ src/components/box-hqs-recents/box-recents.tsx
@@ -4,18 +4,21 @@ import { useEffect, useState } from "react";
 import styles from "./box-hqs.module.css";
 import { hqsService } from "../../../service/WebApiService"; // Atualize se necessário
 import { useRouter } from "next/navigation";
+import { useSearch } from "../../../Context/SearchContext";
 
 interface HQ {
     id: number;
     nome: string;
     capa: string; // Base64 string for the image
     descricao: string;
-    generos: string[]; // Array of genres
+    generos: string; // Array of genres
 }
 
 const BoxRecents = () => {
     const [hqs, setHqs] = useState<HQ[]>([]);
     const router = useRouter();
+    const [filteredHqs, setFilteredHqs] = useState<HQ[]>([]);
+    const { searchQuery } = useSearch();
 
     useEffect(() => {
         hqsService.listarHqs()  
@@ -27,11 +30,20 @@ const BoxRecents = () => {
                 console.error('Erro ao listar hq:', error);
             });
     }, []);
+    useEffect(() => {
+        // Filtra HQs com base na consulta de pesquisa
+        const filtered = hqs.filter(hq => 
+            hq.nome.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            hq.descricao.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            hq.generos.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+        setFilteredHqs(filtered);
+    }, [searchQuery, hqs]);
 
     return (
         <main className={styles.bodyRecents}>
             <h1>HQs Recentes</h1>
-            {hqs.map((hq) => (
+            {filteredHqs.map((hq) => (
                 <div key={hq.id} className={styles.linhaHq}>
                     {hq.capa && <img src={`data:image/jpeg;base64,${hq.capa}`} alt="HQ" />}
                     <div className={styles.descricaoHq}>
